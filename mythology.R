a56de14dcb57a4cfaf4ae601666b40cb0162aed4
Ron Blum <ronblum@gmail.com>
2018-08-16 12:34:37 -0400

    Removed extra line and modified .gitignore for test

--- mythology.R
+++ mythology.R
@@ -11,7 +11,6 @@ deities
 str(deities)
 summary(deities)
 
-
 location <- c("Asgard", "Earth", "Hell", "Mt. Olympus")
 placement <- c("H", "M", "L", "H")
 placement_factor <- factor(placement, ordered = TRUE, levels = c("L", "M", "H"))
